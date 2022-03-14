7b69ddf956a8b5d78abe0c8e01838c7bffef0927
anarotaru20 <anarotaru20@gmail.com>
2022-03-14 10:59:54 +0200

    sfarsitul sem 3

--- s2/src/clase/readers/StudentiReader.java
+++ s2/src/clase/readers/StudentiReader.java
@@ -2,32 +2,32 @@ package clase.readers;
 
 import clase.Aplicant;
 import clase.Student;
-import clase.readers.IReader;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class StudentiReader extends IReader {
 
-    @Override
-    public List<Aplicant> readAplicanti(String file) throws FileNotFoundException {
-        Scanner input = new Scanner(new File(file));
-        input.useDelimiter(",|\n");
-        List<Aplicant> studenti = new ArrayList<Aplicant>();
+    public StudentiReader(String numeFisier) throws FileNotFoundException {
+        super(numeFisier);
+    }
 
-        while (input.hasNext()) {
+    @Override
+    public List<Aplicant> readAplicanti() {
+        super.scanner.useDelimiter(",|\n");
+        List<Aplicant> studenti = new ArrayList<>();
+
+        while (scanner.hasNext()) {
             Student student = new Student();
-            super.citireAplicant(input, student);
-            int an_studii = input.nextInt();
-            String facultate = (input.next()).toString();
-            student.setAn_studii(an_studii);
+            super.citireAplicant(scanner, student);
+            int anStudii = scanner.nextInt();
+            String facultate = (scanner.next());
+            student.setAn_studii(anStudii);
             student.setFacultate(facultate);
             studenti.add(student);
         }
-        input.close();
+        scanner.close();
         return studenti;
     }
 }
