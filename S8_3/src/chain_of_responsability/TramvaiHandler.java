d6352d66de45e447e756cc232aab4f357889850b
anarotaru20 <anarotaru20@gmail.com>
2022-05-09 22:40:05 +0300

    S8 - Chain of Responsability

---
+++ S8_3/src/chain_of_responsability/TramvaiHandler.java
@@ -0,0 +1,17 @@
+package chain_of_responsability;
+
+public class TramvaiHandler extends Handler{
+
+    public TramvaiHandler(int prag) {
+        super(prag);
+    }
+
+    @Override
+    public void afiseazaCuCeSaMearga(int distanta) {
+        if(distanta < prag) {
+            System.out.println("Te poti deplasa cu tramvaiul");
+        } else {
+            nextHandler.afiseazaCuCeSaMearga(distanta);
+        }
+    }
+}
