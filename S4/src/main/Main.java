d0685305010cf881a247b8316ca3683685af8035
anarotaru20 <anarotaru20@gmail.com>
2022-05-09 21:49:21 +0300

    S4 - Prototype

---
+++ S4/src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import prototype.Autobuz;
+import prototype.Tramvai;
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Autobuz autobuz = new Autobuz("Vasile");
+        Tramvai tramvai = new Tramvai("Gigel");
+
+        Autobuz autobuz1 = (Autobuz) autobuz.copiaza();
+        Tramvai tramvai1 = (Tramvai) tramvai.copiaza();
+
+        System.out.println(autobuz.toString());
+        System.out.println(autobuz1.toString());
+        System.out.println(tramvai.toString());
+        System.out.println(tramvai1.toString());
+
+
+    }
+}
