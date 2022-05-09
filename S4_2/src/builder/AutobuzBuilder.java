0d39fc8f3f1c59171c234d98a1feb93eb3076f2f
anarotaru20 <anarotaru20@gmail.com>
2022-05-09 21:53:26 +0300

    S4 - Builder

---
+++ S4_2/src/builder/AutobuzBuilder.java
@@ -0,0 +1,45 @@
+package builder;
+
+public class AutobuzBuilder implements Builder{
+
+    private Autobuz autobuz;
+
+    public AutobuzBuilder() {
+        this.autobuz = new Autobuz("Sofer", 2, true, true, 7, "Statia finala");
+    }
+
+    @Override
+    public Autobuz build() {
+        return autobuz;
+    }
+
+    public AutobuzBuilder setNumeSofer(String numeSofer) {
+        this.autobuz.setNumeSofer(numeSofer);
+        return this;
+    }
+
+    public AutobuzBuilder setNrLinie(int nrLinie) {
+        this.autobuz.setNrLinie(nrLinie);
+        return this;
+    }
+
+    public AutobuzBuilder setPauzaSofer(boolean pauzaSofer) {
+        this.autobuz.setPauzaSofer(pauzaSofer);
+        return this;
+    }
+
+    public AutobuzBuilder setDeschideUsile(boolean deschideUsile) {
+        this.autobuz.setDeschideUsile(deschideUsile);
+        return this;
+    }
+
+    public AutobuzBuilder setOraIncepereProgram(int oraIncepereProgram) {
+        this.autobuz.setOraIncepereProgram(oraIncepereProgram);
+        return this;
+    }
+
+    public AutobuzBuilder setTextDerulat(String textDerulat) {
+        this.autobuz.setTextDerulat(textDerulat);
+        return this;
+    }
+}
